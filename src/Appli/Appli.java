4aa037adb724ababafe3055948f2718d45e1dca5
ClementSefrin <clementsefrin.pro@gmail.com>
2023-10-21 16:47:54 +0200

    complexite

--- src/Appli/Appli.java
+++ src/Appli/Appli.java
@@ -31,18 +31,16 @@ public class Appli {
             a = new AmisHashMap<>();
             CreationMonde.creerMonde(a, i);
             double cUnion1 = ComplexiteHashMap.complexiteUnion(a, i, amis);
-/*
+
             b = new AmisArrayList<>();
             CreationMonde.creerMonde(b, i);
             double cUnion2 = ComplexiteArrayList.complexiteUnion(b, i, amis);
-
- */
 
             c = new UnionFind<>();
             CreationMonde.creerMonde(c, i);
             double cUnion3 = ComplexiteUnionFind.complexiteUnion(c, i, amis);
 
-            System.out.println(i + "\t" + cUnion1 + "\t" + 0 + "\t" + cUnion3);
+            System.out.println(i + "\t" + cUnion1 + "\t" + cUnion2 + "\t" + cUnion3);
         }
         System.out.println();
 
@@ -56,20 +54,17 @@ public class Appli {
             CreationMonde.union(a, i, amis);
             double cFind1 = ComplexiteHashMap.complexiteFind(a, i);
 
-            /*
             b = new AmisArrayList<>();
             CreationMonde.creerMonde(b, i);
             CreationMonde.union(b, i, amis);
             double cFind2 = ComplexiteArrayList.complexiteFind(b, i);
-
-             */
 
             c = new UnionFind<>();
             CreationMonde.creerMonde(c, i);
             CreationMonde.union(c, i, amis);
             double cFind3 = ComplexiteUnionFind.complexiteFind(c, i);
 
-            System.out.println(i + "\t" + cFind1 + "\t" + 0 + "\t" + cFind3);
+            System.out.println(i + "\t" + cFind1 + "\t" + cFind2 + "\t" + cFind3);
         }
         System.out.println();
 
@@ -83,13 +78,12 @@ public class Appli {
             CreationMonde.creerMonde(a, i);
             CreationMonde.union(a, i, amis);
             double cIsoler1 = ComplexiteHashMap.complexiteIsoler(a, i);
-/*
+
             b = new AmisArrayList<>();
             CreationMonde.creerMonde(b, i);
             CreationMonde.union(b, i, amis);
             double cIsoler2 = ComplexiteArrayList.complexiteIsoler(b, i);
 
- */
 
             c = new UnionFind<>();
             CreationMonde.creerMonde(c, i);
@@ -97,7 +91,7 @@ public class Appli {
             double cIsoler3 = ComplexiteUnionFind.complexiteIsoler(c, i);
 
 
-            System.out.println(i + "\t" + cIsoler1 + "\t" + 0 + "\t" + cIsoler3);
+            System.out.println(i + "\t" + cIsoler1 + "\t" + cIsoler2 + "\t" + cIsoler3);
         }
         System.out.println();
     }
